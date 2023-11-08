f7376ab7635709af1da27b7c58d408eb03f36378
TheSainEyereg <oleglogwinow@gmail.com>
2023-11-08 17:15:53 +0300

    Update Animations.js

--- assets/js/modules/Animations.js
+++ assets/js/modules/Animations.js
@@ -2,56 +2,46 @@ import Logger from "./Logger.js";
 const logger = new Logger("JS Animations")
 
 
-function textBrute(id, text, time) {
-	const tick = time/text.length;
-	const element = document.getElementById(id);
+function textBrute(selector, text, time) {
+	const delay = time / text.length;
+	const element = document.querySelector(selector);
 	const chars = "@#$%&/\\|;";
-	let string ="";
-	let i = 0;
+	let i = 0, string = "";
 	function render() {
-		if (i < text.length) {
-			string+=text[i];
-			element.innerHTML = string + chars[Math.floor(Math.random()*chars.length)];
-			if (i === text.length-1) element.innerHTML = string;
-			i++;
-		} else return
-		setTimeout(render, tick);
+		if (++i <= text.length) {
+			if (i === text.length) return element.textContent = string;
+			string += text[i];
+			element.textContent = string + chars[Math.floor(Math.random()*chars.length)];
+			setTimeout(render, delay);
+		}
 	}
 	render();
 }
 
-function titleTimer() {
-	setTimeout(() => {
-		const tick = time/ticks;
-		let cur = ticks;
-		logger.log(`Title counter started for ${tick} ticks every ${tick}ms`);
-		let timer = setInterval(() => {
-			if (cur <= 0) {
-				clearInterval(timer);
-				logger.log("Title counter complete");
-			}
-			document.title = cur;
-			cur--
-		}, tick);
-	},timeout);
+function titleFill(text, time) {
+	const delay = time / text.length;
+	let i = 0, title = ""
+	logger.log(`Title animation started for "${text}" every ${delay}ms`);
+	const timer = setInterval (() => {
+		title = title + text[i];
+		document.title = title;
+		if (++i === text.length) {
+			clearInterval(timer);
+			logger.log("Title animation complete");
+		}
+	}, delay);
 }
 
-function titleFill() {
-	setTimeout(() => {
-		const tick = time/text.length;
-		let cur = 0
-		let title = ""
-		logger.log(`Title animation started for "${text}" every ${tick}ms`);
-		let timer = setInterval (() => {
-			if (cur >= text.length-1) {
-				clearInterval(timer);
-				logger.log("Title animation complete");
-			}
-			title = title + text[cur];
-			document.title = title;
-			cur++;
-		}, tick)
-	}, timeout)
+function titleCountdown(count, delay) {
+	let i = count;
+	logger.log(`Title countdown started for ${count} ticks every ${delay}ms`);
+	let timer = setInterval(() => {
+		document.title = i;
+		if (--i < 0) {
+			clearInterval(timer);
+			logger.log("Title countdown complete");
+		}
+	}, delay);
 }
 
-export {textBrute, titleFill, titleTimer};
+export {textBrute, titleFill, titleCountdown};
